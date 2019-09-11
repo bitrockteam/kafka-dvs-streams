15bd9674d9c108672a8cab2f1b3794090ccf1631
Roberto Maestroni <roberto.maestroni@bitrock.it>
2019-09-11 10:09:12 +0200

    Add startup sleep timeout

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo 'Starting up streams aggregator...' >&2
+
+if [[ $STARTUP_SLEEP_TIMEOUT ]]; then
+  echo "sleeping for $STARTUP_SLEEP_TIMEOUT seconds" >&2
+  sleep $STARTUP_SLEEP_TIMEOUT
+fi
+
+exec ./bin/kafka-flightstream-streams
