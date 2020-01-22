bf6e347c25fee45b8b2f9f997dff0e74eb33ebcb
Daniele Bonelli <daniele.bonelli@bitrock.it>
2020-01-22 12:53:41 +0100

    Optimize flight list stream (#82)
    * optimize flight list stream
    * improve test to verify suppress behaviour

--- src/main/scala/it/bitrock/dvs/streams/topologies/FlightListStream.scala
+++ src/main/scala/it/bitrock/dvs/streams/topologies/FlightListStream.scala
@@ -2,15 +2,17 @@ package it.bitrock.dvs.streams.topologies
 
 import java.util.Properties
 
-import it.bitrock.dvs.streams._
-import it.bitrock.dvs.streams.StreamProps.streamProperties
-import it.bitrock.dvs.streams.config.AppConfig
 import it.bitrock.dvs.model.avro._
+import it.bitrock.dvs.streams.StreamProps.streamProperties
+import it.bitrock.dvs.streams._
+import it.bitrock.dvs.streams.config.AppConfig
 import org.apache.kafka.common.serialization.Serde
 import org.apache.kafka.streams.Topology
 import org.apache.kafka.streams.kstream.TimeWindows
 import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala.StreamsBuilder
+import org.apache.kafka.streams.scala.kstream.Suppressed
+import org.apache.kafka.streams.scala.kstream.Suppressed.BufferConfig
 
 object FlightListStream {
 
@@ -25,8 +27,13 @@ object FlightListStream {
     streamsBuilder
       .stream[String, FlightReceived](config.kafka.topology.flightReceivedTopic)
       .groupBy((_, _) => AllRecordsKey)
-      .windowedBy(TimeWindows.of(duration2JavaDuration(config.kafka.topology.aggregationTimeWindowSize)))
-      .aggregate(FlightReceivedList())((_, v, agg) => FlightReceivedList(agg.elements :+ v))
+      .windowedBy(
+        TimeWindows
+          .of(duration2JavaDuration(config.kafka.topology.aggregationTimeWindowSize))
+          .grace(duration2JavaDuration(config.kafka.topology.aggregationTimeWindowGrace))
+      )
+      .aggregate(FlightReceivedList())((_, v, agg) => FlightReceivedList(v +: agg.elements))
+      .suppress(Suppressed.untilWindowCloses(BufferConfig.unbounded()))
       .toStream
       .map((k, v) => (k.window.start.toString, v))
       .to(config.kafka.topology.flightReceivedListTopic)
