706415a138fcf6386b88a1994b9c227965ba6e0d
Simone Esposito <simoexpo@outlook.com>
2020-02-17 16:40:46 +0100

    Filter out airplanes near arrival airport

--- src/main/scala/it/bitrock/dvs/streams/topologies/FlightListStream.scala
+++ src/main/scala/it/bitrock/dvs/streams/topologies/FlightListStream.scala
@@ -1,9 +1,7 @@
 package it.bitrock.dvs.streams.topologies
 
-import java.time.Instant
-import java.util.{Properties, UUID}
+import java.util.Properties
 
-import it.bitrock.dvs.model.avro.monitoring.FlightReceivedListComputationStatus
 import it.bitrock.dvs.model.avro.{FlightReceived, FlightReceivedList}
 import it.bitrock.dvs.streams.StreamProps.streamProperties
 import it.bitrock.dvs.streams._
@@ -15,17 +13,22 @@ import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.scala.kstream.Suppressed.BufferConfig
 import org.apache.kafka.streams.scala.kstream.{Produced, Suppressed}
+import it.bitrock.dvs.streams.topologies.StreamOps._
 
 object FlightListStream {
-  final val AllRecordsKey: String = "all"
+  final val AllRecordsKey: String            = "all"
+  final val EarthRadius: Double              = 6371.01
+  final val MaxDepartureAirportDistance: Int = 10
+
+  sealed trait FlightStatus extends Product with Serializable
+  case object Landed        extends FlightStatus
+  case object EnRoute       extends FlightStatus
 
   def buildTopology(config: AppConfig, kafkaStreamsOptions: KafkaStreamsOptions): List[(Topology, Properties)] = {
     implicit val StringKeySerde: Serde[String]                           = kafkaStreamsOptions.stringKeySerde
     implicit val IntKeySerde: Serde[Int]                                 = kafkaStreamsOptions.intKeySerde
     implicit val flightReceivedEventSerde: Serde[FlightReceived]         = kafkaStreamsOptions.flightReceivedEventSerde
     implicit val flightReceivedListEventSerde: Serde[FlightReceivedList] = kafkaStreamsOptions.flightReceivedListEventSerde
-    implicit val computationStatusSerde: Serde[FlightReceivedListComputationStatus] =
-      kafkaStreamsOptions.flightReceivedListComputationStatusSerde
 
     def partitioner(key: String): Int =
       Math.abs(key.hashCode % config.kafka.topology.flightReceivedPartitionerTopic.partitions)
@@ -36,7 +39,7 @@ object FlightListStream {
       )
 
     val streamsBuilder = new StreamsBuilder
-    streamsBuilder
+    val (landedFlights, enRouteFlights) = streamsBuilder
       .stream[String, FlightReceived](config.kafka.topology.flightReceivedTopic.name)
       .selectKey((k, _) => partitioner(k))
       .through(config.kafka.topology.flightReceivedPartitionerTopic.name)(fixedPartitioner)
@@ -60,23 +63,30 @@ object FlightListStream {
       .toStream
       .map((k, v) => (k.window.start.toString, v))
       .through(config.kafka.topology.flightReceivedListTopic.name)
-      .map((k, v) => (UUID.randomUUID().toString, computationStatus(k, v)))
-      .to(config.kafka.monitoring.flightReceivedList.topic)
+      .flatMap { (k, v) =>
+        val (g, b) = v.elements.partition(distanceToDestination(_) < MaxDepartureAirportDistance)
+        List((k, (Landed, FlightReceivedList(g))), (k, (EnRoute, FlightReceivedList(b))))
+      }
+      .split(
+        (_, v) => v._1 == Landed,
+        (_, v) => v._1 == EnRoute
+      )
+
+    landedFlights.mapValues(_._2).to(config.kafka.topology.flightLandedListTopic.name)
+    enRouteFlights.mapValues(_._2).to(config.kafka.topology.flightEnRouteListTopic.name)
 
     val props = streamProperties(config.kafka, config.kafka.topology.flightReceivedListTopic.name)
     List((streamsBuilder.build(props), props))
   }
 
-  private def computationStatus(windowStart: String, v: FlightReceivedList): FlightReceivedListComputationStatus = {
-    val elements = v.elements.size
-    val average  = v.elements.map(_.updated.toEpochMilli).sum / elements
-    FlightReceivedListComputationStatus(
-      windowTime = Instant.ofEpochMilli(windowStart.toLong),
-      emissionTime = Instant.now(),
-      minUpdated = v.elements.minBy(_.updated).updated,
-      maxUpdated = v.elements.maxBy(_.updated).updated,
-      averageUpdated = Instant.ofEpochMilli(average),
-      windowElements = elements
+  private def distanceToDestination(flight: FlightReceived): Double = {
+    val latRad1 = Math.toRadians(flight.geography.latitude)
+    val lonRad1 = Math.toRadians(flight.geography.longitude)
+    val latRad2 = Math.toRadians(flight.arrivalAirport.latitude)
+    val lonRad2 = Math.toRadians(flight.arrivalAirport.longitude)
+
+    EarthRadius * Math.acos(
+      Math.sin(latRad1) * Math.sin(latRad2) + Math.cos(latRad1) * Math.cos(latRad2) * Math.cos(lonRad1 - lonRad2)
     )
   }
 }
--- src/test/scala/it/bitrock/dvs/streams/topologies/monitoring/FlightReceivedListComputationStatusStreamsSpec.scala
+++ src/test/scala/it/bitrock/dvs/streams/topologies/monitoring/FlightReceivedListComputationStatusStreamsSpec.scala
@@ -1,9 +1,12 @@
 package it.bitrock.dvs.streams.topologies.monitoring
 
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 
+import it.bitrock.dvs.model.avro.FlightReceivedList
 import it.bitrock.dvs.model.avro.monitoring.FlightReceivedListComputationStatus
 import it.bitrock.dvs.streams.CommonSpecUtils._
+import it.bitrock.dvs.streams.TestValues
 import it.bitrock.kafkacommons.serialization.ImplicitConversions._
 import it.bitrock.testcommons.Suite
 import net.manub.embeddedkafka.schemaregistry.EmbeddedKafkaConfig
@@ -16,42 +19,87 @@ class FlightReceivedListComputationStatusStreamsSpec
     extends Suite
     with AnyWordSpecLike
     with EmbeddedKafkaStreams
-    with OptionValues {
+    with OptionValues
+    with TestValues {
+
   "FlightReceivedListComputationStatusStreams" should {
-    "produce record in delay topic for too old records" in ResourceLoaner.withFixture {
+    "produce record of FlightReceivedListComputationStatus in delay topic for too old records" in ResourceLoaner.withFixture {
       case Resource(embeddedKafkaConfig, appConfig, kafkaStreamsOptions, _) =>
-        implicit val embKafkaConfig: EmbeddedKafkaConfig = embeddedKafkaConfig
-        implicit val keySerde: Serde[String]             = kafkaStreamsOptions.stringKeySerde
+        implicit val embKafkaConfig: EmbeddedKafkaConfig                     = embeddedKafkaConfig
+        implicit val keySerde: Serde[String]                                 = kafkaStreamsOptions.stringKeySerde
+        implicit val flightReceivedListEventSerde: Serde[FlightReceivedList] = kafkaStreamsOptions.flightReceivedListEventSerde
         implicit val valueSerde: Serde[FlightReceivedListComputationStatus] =
           kafkaStreamsOptions.flightReceivedListComputationStatusSerde
 
         val now = Instant.now()
+
+        val firstMessage = FlightReceivedEvent.copy(
+          iataNumber = "iata1",
+          icaoNumber = "icao1",
+          updated = now.minus(1, ChronoUnit.SECONDS)
+        )
+
+        val secondMessage = FlightReceivedEvent.copy(
+          iataNumber = "iata2",
+          icaoNumber = "icao2",
+          updated = now
+        )
+
+        val thirdMessage = FlightReceivedEvent.copy(
+          iataNumber = "iata3",
+          icaoNumber = "icao3",
+          updated = now.plus(1, ChronoUnit.SECONDS)
+        )
+
+        val receivedList = FlightReceivedList(List(firstMessage, secondMessage, thirdMessage))
+
         val delayedWindowTime =
           now.minusMillis(appConfig.kafka.monitoring.flightReceivedList.allowedDelay.toMillis + 1000)
-        val delayedComputationStatus = FlightReceivedListComputationStatus(delayedWindowTime, now, now, now, now, 10)
+        val delayedComputationStatus =
+          FlightReceivedListComputationStatus(delayedWindowTime, now, firstMessage.updated, thirdMessage.updated, now, 3)
 
         val topology = FlightReceivedListComputationStatusStreams.buildTopology(appConfig, kafkaStreamsOptions).map(_._1)
         val receivedRecords =
           ResourceLoaner.runAll(topology) { _ =>
             publishToKafka(
-              appConfig.kafka.monitoring.flightReceivedList.topic,
-              "unused",
-              FlightReceivedListComputationStatus(now, now, now, now, now, 5)
+              appConfig.kafka.topology.flightReceivedListTopic.name,
+              delayedWindowTime.toEpochMilli.toString,
+              receivedList
             )
 
-            publishToKafka(appConfig.kafka.monitoring.flightReceivedList.topic, "unused", delayedComputationStatus)
+            val computationStatusMessagesMap = consumeNumberKeyedMessagesFromTopics[String, FlightReceivedListComputationStatus](
+              topics = Set(appConfig.kafka.monitoring.flightReceivedList.topic),
+              number = 1,
+              timeout = ConsumerPollTimeout
+            )
 
-            val messagesMap = consumeNumberKeyedMessagesFromTopics[String, FlightReceivedListComputationStatus](
+            val delayMessagesMap = consumeNumberKeyedMessagesFromTopics[String, FlightReceivedListComputationStatus](
               topics = Set(appConfig.kafka.monitoring.flightReceivedList.delayTopic),
               number = 1,
               timeout = ConsumerPollTimeout
             )
 
-            messagesMap(appConfig.kafka.monitoring.flightReceivedList.delayTopic).map(_._2)
+            (
+              computationStatusMessagesMap(appConfig.kafka.monitoring.flightReceivedList.topic).map(_._2),
+              delayMessagesMap(appConfig.kafka.monitoring.flightReceivedList.delayTopic).map(_._2)
+            )
           }
 
-        receivedRecords should have size 1
-        receivedRecords.head shouldBe delayedComputationStatus
+        val (computationStatus, delay) = receivedRecords
+
+        computationStatus should have size 1
+        val cs = computationStatus.head
+        cs.minUpdated shouldBe firstMessage.updated
+        cs.maxUpdated shouldBe thirdMessage.updated
+        cs.averageUpdated shouldBe now
+        cs.windowElements shouldBe 3
+
+        delay should have size 1
+        delay.head.windowTime shouldBe delayedComputationStatus.windowTime
+        delay.head.minUpdated shouldBe delayedComputationStatus.minUpdated
+        delay.head.maxUpdated shouldBe delayedComputationStatus.maxUpdated
+        delay.head.averageUpdated shouldBe delayedComputationStatus.averageUpdated
+        delay.head.windowElements shouldBe delayedComputationStatus.windowElements
     }
   }
 }
