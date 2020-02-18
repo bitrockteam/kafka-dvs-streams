6b3f14bac3c071107eaa8e2858060a3222785644
Daniele Bonelli <daniele.bonelli@bitrock.it>
2020-02-18 15:53:45 +0100

    added first working solution for interpolated records emission

---
+++ src/main/scala/it/bitrock/dvs/streams/topologies/FlightInterpolatedListStream.scala
@@ -0,0 +1,88 @@
+package it.bitrock.dvs.streams.topologies
+
+import java.util.Properties
+
+import it.bitrock.dvs.model.avro.{FlightReceived, FlightReceivedList}
+import it.bitrock.dvs.streams.StreamProps.streamProperties
+import it.bitrock.dvs.streams._
+import it.bitrock.dvs.streams.config.AppConfig
+import org.apache.kafka.common.serialization.Serde
+import org.apache.kafka.streams.kstream.Transformer
+import org.apache.kafka.streams.processor._
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala.StreamsBuilder
+import org.apache.kafka.streams.state.{KeyValueStore, Stores}
+import org.apache.kafka.streams.{KeyValue, Topology}
+
+import scala.concurrent.duration._
+
+object FlightInterpolatedListStream {
+  private val currentSnapshot = "currentSnapshot"
+
+  def buildTopology(config: AppConfig, kafkaStreamsOptions: KafkaStreamsOptions): List[(Topology, Properties)] = {
+    implicit val stringKeySerde: Serde[String]                           = kafkaStreamsOptions.stringKeySerde
+    implicit val flightReceivedListEventSerde: Serde[FlightReceivedList] = kafkaStreamsOptions.flightReceivedListEventSerde
+
+    val streamsBuilder = new StreamsBuilder
+
+    val stateStore = Stores.keyValueStoreBuilder(
+      Stores.persistentKeyValueStore("FlightListInterpolationStateStore"),
+      stringKeySerde,
+      flightReceivedListEventSerde
+    )
+
+    streamsBuilder.addStateStore(stateStore)
+
+    streamsBuilder
+      .stream[String, FlightReceivedList](config.kafka.topology.flightEnRouteListTopic.name)
+      .transform[String, FlightReceivedList](
+        () => interpolationTransformer(stateStore.name, config.kafka.topology.interpolationInterval),
+        stateStore.name
+      )
+      .to(config.kafka.topology.flightInterpolatedListTopic.name)
+
+    val props = streamProperties(config.kafka, config.kafka.topology.flightReceivedListTopic.name)
+    List((streamsBuilder.build(props), props))
+  }
+
+  private def interpolationTransformer(
+      stateStoreName: String,
+      interpolationInterval: FiniteDuration
+  ): Transformer[String, FlightReceivedList, KeyValue[String, FlightReceivedList]] =
+    new Transformer[String, FlightReceivedList, KeyValue[String, FlightReceivedList]] {
+      private var processorContext: ProcessorContext                       = _
+      private var keyValueStore: KeyValueStore[String, FlightReceivedList] = _
+      private var scheduledTask: Cancellable                               = _
+
+      override def init(context: ProcessorContext): Unit = {
+        processorContext = context
+        keyValueStore = context.getStateStore(stateStoreName).asInstanceOf[KeyValueStore[String, FlightReceivedList]]
+        scheduledTask = context.schedule(
+          duration2JavaDuration(interpolationInterval),
+          PunctuationType.WALL_CLOCK_TIME,
+          interpolationPunctuator(context, keyValueStore)
+        )
+      }
+
+      override def transform(key: String, value: FlightReceivedList): KeyValue[String, FlightReceivedList] = {
+        keyValueStore.put(currentSnapshot, value)
+        KeyValue.pair(key, value)
+      }
+
+      override def close(): Unit = scheduledTask.cancel()
+
+    }
+
+  private def interpolationPunctuator(
+      processorContext: ProcessorContext,
+      keyValueStore: KeyValueStore[String, FlightReceivedList]
+  ): Punctuator =
+    (timestamp: Long) =>
+      processorContext.forward(
+        timestamp.toString,
+        FlightReceivedList(keyValueStore.get(currentSnapshot).elements.map(interpolate))
+      )
+
+  def interpolate(flightReceived: FlightReceived): FlightReceived = flightReceived
+
+}
---
+++ src/test/scala/it/bitrock/dvs/streams/topologies/FlightInterpolatedListStreamSpec.scala
@@ -0,0 +1,77 @@
+package it.bitrock.dvs.streams.topologies
+
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+import it.bitrock.dvs.model.avro._
+import it.bitrock.dvs.streams.CommonSpecUtils._
+import it.bitrock.dvs.streams.TestValues
+import it.bitrock.kafkacommons.serialization.ImplicitConversions._
+import it.bitrock.testcommons.Suite
+import net.manub.embeddedkafka.schemaregistry._
+import net.manub.embeddedkafka.schemaregistry.streams.EmbeddedKafkaStreams
+import org.apache.kafka.common.serialization.Serde
+import org.scalatest.OptionValues
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class FlightInterpolatedListStreamSpec
+    extends Suite
+    with AnyWordSpecLike
+    with EmbeddedKafkaStreams
+    with OptionValues
+    with TestValues {
+  "FlightInterpolatedListStream" should {
+    "produce interpolated record of FlightReceivedList in interpolated topic" in ResourceLoaner.withFixture {
+      case Resource(embeddedKafkaConfig, appConfig, kafkaStreamsOptions, _) =>
+        implicit val embKafkaConfig: EmbeddedKafkaConfig                     = embeddedKafkaConfig
+        implicit val keySerde: Serde[String]                                 = kafkaStreamsOptions.stringKeySerde
+        implicit val flightReceivedListEventSerde: Serde[FlightReceivedList] = kafkaStreamsOptions.flightReceivedListEventSerde
+
+        val expectedMessages = (ConsumerPollTimeout / appConfig.kafka.topology.interpolationInterval).toInt
+
+        val now = Instant.now()
+
+        val firstMessage = FlightReceivedEvent.copy(
+          iataNumber = "iata1",
+          icaoNumber = "icao1",
+          updated = now.minus(1, ChronoUnit.SECONDS)
+        )
+
+        val secondMessage = FlightReceivedEvent.copy(
+          iataNumber = "iata2",
+          icaoNumber = "icao2",
+          updated = now
+        )
+
+        val thirdMessage = FlightReceivedEvent.copy(
+          iataNumber = "iata3",
+          icaoNumber = "icao3",
+          updated = now.plus(1, ChronoUnit.SECONDS)
+        )
+
+        val receivedList = FlightReceivedList(List(firstMessage, secondMessage, thirdMessage))
+
+        val topology = FlightInterpolatedListStream.buildTopology(appConfig, kafkaStreamsOptions).map(_._1)
+        val receivedRecords = ResourceLoaner.runAll(topology) { _ =>
+          publishToKafka(
+            appConfig.kafka.topology.flightEnRouteListTopic.name,
+            now.toEpochMilli.toString,
+            receivedList
+          )
+
+          val messages = consumeNumberKeyedMessagesFromTopics[String, FlightReceivedList](
+            topics = Set(appConfig.kafka.topology.flightInterpolatedListTopic.name),
+            number = expectedMessages,
+            timeout = ConsumerPollTimeout
+          )
+
+          messages(appConfig.kafka.topology.flightInterpolatedListTopic.name)
+
+        }
+
+        receivedRecords should have size expectedMessages
+
+    }
+
+  }
+}
