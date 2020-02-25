2d2cd6b8409f0c09f0ef602685326da52b5ad16d
Massimo Siani <massimosiani@users.noreply.github.com>
2020-02-25 21:26:10 +0100

    update the tools version (#146)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,8 +23,7 @@ object Dependencies {
     lazy val ScalaTestAutofix    = "3.1.0.0"
     lazy val Slf4j               = "1.7.30"
     lazy val TestCommons         = "0.0.8"
-    lazy val ScalaCheck          = "1.14.3"
-    lazy val ScalaTestPlus       = "3.1.0.0-RC2"
+    lazy val ScalaTestPlus       = "3.1.1.1"
 
   }
 
@@ -55,8 +54,7 @@ object Dependencies {
     "com.softwaremill.diffx"  %% "diffx-scalatest"                % Versions.Diffx,
     "io.github.embeddedkafka" %% "embedded-kafka-schema-registry" % Versions.ConfluentPlatform,
     "it.bitrock"              %% "test-commons"                   % Versions.TestCommons,
-    "org.scalacheck"          %% "scalacheck"                     % Versions.ScalaCheck,
-    "org.scalatestplus"       %% "scalatestplus-scalacheck"       % Versions.ScalaTestPlus
+    "org.scalatestplus"       %% "scalacheck-1-14"                % Versions.ScalaTestPlus
   ).map(_ % Test)
 
   lazy val excludeDeps: Seq[ExclusionRule] = Seq(
