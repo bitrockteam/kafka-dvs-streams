c75898678a0859cf8a48ae9ca87db2addc239ec3
Scala Steward <me@scala-steward.org>
2020-04-12 18:09:13 +0200

    Update diffx-scalatest to 0.3.27

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   object Versions {
     lazy val Scala               = "2.12.10"
     lazy val ConfluentPlatform   = "5.4.0"
-    lazy val Diffx               = "0.3.20"
+    lazy val Diffx               = "0.3.27"
     lazy val JakartaWsRs         = "2.1.6"
     lazy val Kafka               = "2.4.0"
     lazy val KafkaCommons        = "0.0.8"
