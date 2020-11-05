9eec1dc4975db2777196f477b5c8e7256a44d164
Scala Steward <me@scala-steward.org>
2020-11-05 18:21:12 +0100

    Update kafka-streams-scala to 2.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     lazy val EmbeddedKafka       = "5.4.1.2"
     lazy val Diffx               = "0.3.29"
     lazy val JakartaWsRs         = "2.1.6"
-    lazy val Kafka               = "2.4.1"
+    lazy val Kafka               = "2.6.0"
     lazy val KafkaCommons        = "0.0.8"
     lazy val KafkaDVSAvroSchemas = "1.0.20"
     lazy val LogbackClassic      = "1.2.3"
