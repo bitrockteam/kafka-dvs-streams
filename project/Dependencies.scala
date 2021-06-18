c73fb835c50f66232b3cb80b971377cb582750b8
Scala Steward <me@scala-steward.org>
2021-06-18 09:34:39 +0200

    Update log4j-over-slf4j to 1.7.31

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     lazy val ScalafixSortImports = "0.5.5"
     lazy val ScalaLogging        = "3.9.2"
     lazy val ScalaTestAutofix    = "3.1.0.1"
-    lazy val Slf4j               = "1.7.30"
+    lazy val Slf4j               = "1.7.31"
     lazy val TestCommons         = "0.0.8"
     lazy val ScalaTestPlus       = "3.1.1.1"
 
