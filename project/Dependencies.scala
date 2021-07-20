c0595bca91b2f3db24586d2febcddc920624e65f
Scala Steward <me@scala-steward.org>
2021-07-20 14:36:43 +0200

    Update pureconfig to 0.16.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     lazy val KafkaCommons        = "0.0.8"
     lazy val KafkaDVSAvroSchemas = "1.0.20"
     lazy val LogbackClassic      = "1.2.4"
-    lazy val PureConfig          = "0.12.3"
+    lazy val PureConfig          = "0.16.0"
     lazy val ScalacheckShapeless = "1.2.5"
     lazy val ScalafixSortImports = "0.5.5"
     lazy val ScalaLogging        = "3.9.4"
