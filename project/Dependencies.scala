316bcb6b0c5317e3a6cea94f1e7e9a0824971d71
Scala Steward <scala_steward@virtuslab.com>
2023-04-15 12:49:55 +0000

    Update pureconfig to 0.17.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     lazy val KafkaCommons        = "0.0.8"
     lazy val KafkaDVSAvroSchemas = "1.0.20"
     lazy val LogbackClassic      = "1.2.10"
-    lazy val PureConfig          = "0.17.1"
+    lazy val PureConfig          = "0.17.3"
     lazy val ScalacheckShapeless = "1.3.0"
     lazy val ScalafixSortImports = "0.6.1"
     lazy val ScalaLogging        = "3.9.4"
